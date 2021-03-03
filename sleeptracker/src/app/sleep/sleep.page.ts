5c95e0f7a8ed0a6efc6522c8004b8026d18fb781
Cheryll G <32274937+cheryllg@users.noreply.github.com>
2021-03-03 22:05:47 +0800

    Update sleep.page.ts

--- sleeptracker/src/app/sleep/sleep.page.ts
+++ sleeptracker/src/app/sleep/sleep.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SleepService } from '../services/sleep.service';
+import { Router } from '@angular/router';
+import { SleepData } from '../data/sleep-data';
+import { OvernightSleepData } from '../data/overnight-sleep-data';
+import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sleep',
@@ -6,10 +13,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sleep.page.scss'],
 })
 export class SleepPage implements OnInit {
+  sleepStr:OvernightSleepData[];
+  sleepTime:Date = new Date();
+  wakeTime:Date = new Date();
 
-  constructor() { }
+  constructor(public sleepService:SleepService, private route: Router, public modalController: ModalController) { }
 
+  
   ngOnInit() {
+    this.sleepStr = SleepService.AllOvernightData;
   }
 
+  logdata(){
+    let sleepData: OvernightSleepData = new OvernightSleepData(this.sleepTime, this.wakeTime);
+    this.sleepService.logOvernightData(sleepData);
+  }
 }
