d07a0cafbd648a8dfceac6249c38727fcd3db907
Nicole Loh <nicole.aloh@gmail.com>
2021-03-03 14:27:04 -0800

    Add sleepiness history

--- sleeptracker/src/app/history/history.page.ts
+++ sleeptracker/src/app/history/history.page.ts
@@ -12,12 +12,64 @@ import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
 export class HistoryPage implements OnInit {
   sleepArray: OvernightSleepData[];
   sleepinessArray: StanfordSleepinessData[];
+  sleepHistory: Array<String>[];
+  sleepinessHistory: Array<String>[];
 
-  constructor() { }
+  constructor(public sleepService:SleepService) { }
 
   ngOnInit() {
-    this.sleepArray = SleepService.AllOvernightData;
-    this.sleepinessArray = SleepService.AllSleepinessData;
+    this.sleepArray = this.allOvernightData;
+    this.sleepinessArray = this.allSleepinessData;
+    this.sleepinessHistory = this.formatSleepinessHistory;
+  }
+
+  get allSleepData() {
+    return SleepService.AllSleepData;
+  }
+
+  get allOvernightData() {
+    return SleepService.AllOvernightData;
+  }
+
+  get allSleepinessData() {
+    return SleepService.AllSleepinessData;
+  }
+
+  get totalHours() {
+    return 0;
+  }
+
+  options = {
+      slidesPerView: 1.3,
+  }
+
+  get recordedTime() {
+    //time = data.loggedAt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
+    return "lalala";
+  }
+
+  get formatSleepinessHistory(){
+    var currentSleepHistory = [];
+
+    console.log(this.sleepinessArray);
+
+    for (var i in this.sleepinessArray) {
+      var currentSleepinessData = this.sleepinessArray[i];
+      var tanggal = currentSleepinessData.loggedAt.toLocaleDateString('en-US', {month: 'long', day: 'numeric' });
+
+      var jam = currentSleepinessData.loggedAt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      var level = currentSleepinessData.summaryString();
+
+      var sleepLog = [tanggal, jam, level];
+      currentSleepHistory.push(sleepLog);
+      
+    }
+
+    // First data: date logged
+    // Second data: time logged
+    // Third data: level of sleepiness
+
+    return currentSleepHistory;
   }
 
 }
