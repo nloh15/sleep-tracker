d49225bcc0264150c1e11196e02a39cf69c47e52
Cheryll Gunawan <cheryllgunawan@gmail.com>
2021-03-07 12:35:58 +0800

    bar chart

--- sleeptracker/src/app/history/history.page.ts
+++ sleeptracker/src/app/history/history.page.ts
@@ -131,7 +131,35 @@ export class HistoryPage implements OnInit {
 
     return num;
   }
+  /*
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
 
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }*/
 
 
 }
